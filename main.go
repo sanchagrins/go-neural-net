fb1ba9852e2e6690ff950b001615768543b6faf2
sanchagrins <sanchagrins@gmail.com>
2017-09-05 23:48:18 +0900

    Added scalar subtraction

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 import "math/rand"
 
+func nonlin(x[4][1]float64, deriv bool) [4][1]float64 {
+	if deriv == true {
+		return subScalar(1.0, x)
+	} else {
+		return x
+	}
+}
+
 func main() {
 	var inData =[4][3]float64{
 		{0,0,1},
@@ -10,9 +18,9 @@ func main() {
 		{1,0,1},
 		{1,1,1},
 	}
-	var outData =[1][4]int{
-		{0,0,1,1},
-	}
+	//var outData =[1][4]int{
+	//	{0,0,1,1},
+	//}
 
 	rand.Seed(1)
 
@@ -22,15 +30,15 @@ func main() {
 		 {(rand.Float64()*2)-1},
 	 }
 	var result[4][1]float64
+	var deriv bool = true
 
-	fmt.Println(inData)
-	fmt.Println(outData)
-
-	fmt.Println(weights)
 	for i:=0; i<=10000; i++ {
 		//l0 := inData
 	}
 
-	result = dot(inData, weights)
+	result = matrixVector(inData, weights)
+	fmt.Println(result)
+
+	result = nonlin(result, deriv)
 	fmt.Println(result)
 }
--- util.go
+++ util.go
@@ -1,6 +1,6 @@
 package main
 
-func dot(inData[4][3]float64, weights[3][1]float64) [4][1]float64{
+func matrixVector(inData[4][3]float64, weights[3][1]float64) [4][1]float64{
 	var tmp[4][3]float64
 	var result[4][1]float64
 
@@ -17,5 +17,16 @@ func dot(inData[4][3]float64, weights[3][1]float64) [4][1]float64{
              }
 	     result[i][0] =  sum
         }
+	return result
+}
+
+func subScalar(scalar float64, inData[4][1]float64) [4][1]float64{
+	var result[4][1]float64
+
+	for i:= 0; i<4; i++ {
+		for j :=0; j<1; j++ {
+			result[i][j] = scalar-inData[i][j]
+		}
+	}
 	return result
 }
