b139183199ae5b39ae810b35f7c4f663b0e8f389
sanchagrins <sanchagrins@gmail.com>
2017-12-20 22:22:15 +0900

    Added sigmoid and elementMult unit test

--- util_test.go
+++ util_test.go
@@ -14,3 +14,27 @@ func Test_subScalar(t *testing.T) {
 	}
 }
 
+func Test_sigmoid(t *testing.T) {
+	v := [][]float64{{0},{0},{0},{0}}
+
+	val := sigmoid(v)
+
+	for i:=0;i<len(val);i++ {
+		if val[i][0] != 0.5 {
+			t.Fatalf("Error: Expected '0.5', found '%v'", val[i][0])
+		}
+	}
+}
+
+func Test_elementMult(t *testing.T) {
+	v := [][]float64{{2},{2},{2},{2}}
+
+	val := elementMult(v,v)
+
+	for i:=0;i<len(val);i++ {
+		if val[i][0] != 4 {
+			t.Fatalf("Error: Expected '4', found '%v'", val[i][0])
+		}
+
+	}
+}
